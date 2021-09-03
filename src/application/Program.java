7bbdfcb3b231c337aed1aee818f1a24befe8bb29
wilnerclaro <wilnerclaro@gmail.com>
2021-09-03 11:01:40 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -11,12 +14,19 @@ public class Program {
 	public static void main(String[] args) {
 	
 	SimpleDateFormat sdf= new SimpleDateFormat("dd/MM/yyyy");
-		
-	System.out.println("=== TEST 1: seller findById ===");
 	SellerDao sellerDao = DaoFactory.createSellerDao();
-	Seller seller = sellerDao.findById(3);
 	
+	System.out.println("=== TEST 1: seller findById ===");
+	Seller seller = sellerDao.findById(3);
 	System.out.println(seller);
+	
+	System.out.println("\n=== TEST : seller findByDepartment===");
+	Department department = new Department(2, null);
+	List<Seller> list = sellerDao.findByDepartment(department);
+	for (Seller seller2 : list) {
+		System.out.println(seller2);
+	}
+	
 	}
 
 }
