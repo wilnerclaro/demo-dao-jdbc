77d61b5fe213ddd2b537a3fba7fa526d9789c54a
wilnerclaro <wilnerclaro@gmail.com>
2021-09-03 09:25:10 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,9 @@
 package application;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -14,13 +12,11 @@ public class Program {
 	
 	SimpleDateFormat sdf= new SimpleDateFormat("dd/MM/yyyy");
 		
-	Department obj = new Department(1, "Books");
-	System.out.println(obj);
-
-	Seller seller = new Seller(1, "Bob Brow", "bob@gmail.com",  new Date(), 3000.0, obj);
-	System.out.println(seller);
 	
 	SellerDao sellerDao = DaoFactory.createSellerDao();
+	Seller seller = sellerDao.findById(3);
+	
+	System.out.println(seller);
 	}
 
 }
