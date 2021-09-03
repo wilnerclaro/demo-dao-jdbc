0b50820f6da9de2947676d51db63704a9aa27765
wilnerclaro <wilnerclaro@gmail.com>
2021-09-03 08:22:05 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -17,6 +19,8 @@ public class Program {
 
 	Seller seller = new Seller(1, "Bob Brow", "bob@gmail.com",  new Date(), 3000.0, obj);
 	System.out.println(seller);
+	
+	SellerDao sellerDao = DaoFactory.createSellerDao();
 	}
 
 }
