ebc11ae0dd2d6d7dff220523d787cf870d8c35c1
wilnerclaro <wilnerclaro@gmail.com>
2021-09-03 12:17:22 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,15 @@ public class Program {
 	sellerDao.insert(newSeller);
 	System.out.println("Inserted! New id = " + newSeller.getId());
 	
+	System.out.println("\n=== TEST 6: seller update ===");
+	seller = sellerDao.findById(1);
+	seller.setName("Martha Waine");
+	sellerDao.update(seller);
+	System.out.println("Updated Completed!");
 	}
+	
+	
+	
+	
 
 }
